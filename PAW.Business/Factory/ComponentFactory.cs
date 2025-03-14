57788778078e44bc2774667c642a7156cbdbd218
kapauli98 <kapauli98@gmail.com>
2025-03-14 17:59:03 -0600

    Semana 08 - Factory pattern

---
+++ PAW.Business/Factory/ComponentFactory.cs
@@ -0,0 +1,37 @@
+﻿using PAW.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAW.Business.Factory
+{
+
+    public class ComponentFactory 
+    {
+        public Component CreateComponent<T>() where T : Component, new()
+        {
+            return new T();
+        }
+
+        public ComponentFactory SetProperty(Component component, string url, IEnumerable<Component> items)
+        {
+            component.Url = url;
+            component.Data = items;
+            return this;
+        }
+
+
+        public ComponentFactory SetProperty2()
+        {
+            return this;
+        }
+
+        public ComponentFactory SetProperty3()
+        {
+            return this;
+        }
+    }
+}
