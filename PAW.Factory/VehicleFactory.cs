8c4db466cb7c087bde455c8dda59fbb1c6d66fc9
bryandavila <badmdavila@gmail.com>
2025-03-19 19:58:46 -0600

    Se agregan los PAW.Factory

---
+++ PAW.Factory/VehicleFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAW.Factory
+{
+    public class VehicleFactory
+    {
+        public static IVehicle Create(string name)
+        {
+            return name switch
+            {
+                "toyota" => new Toyota().Create(),
+                "ford" => new Ford().Create(),
+                "honda" => new Honda().Create(),
+                _ => new Toyota(),
+            };
+        }
+    }
+}
---
+++ PAW.FactoryTests/VehicleFactoryTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PAW.Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAW.Factory.Tests
+{
+    [TestClass()]
+    public class VehicleFactoryTests
+    {
+        [TestMethod()]
+        public void CreateToyotaTest()
+        {
+            var vehicle = VehicleFactory.Create("toyota");
+            Assert.IsNotNull(vehicle);
+            Assert.IsTrue(vehicle.Model == "Generic");
+        }
+
+        [TestMethod()]
+        public void CreateFordTest()
+        {
+            var vehicle = VehicleFactory.Create("ford");
+            Assert.IsNotNull(vehicle);
+            Assert.IsTrue(vehicle.Model == "Generic");
+        }
+
+        [TestMethod()]
+        public void CreateHondaTest()
+        {
+            var vehicle = VehicleFactory.Create("honda");
+            Assert.IsNotNull(vehicle);
+            Assert.IsTrue(vehicle.Model == "Generic");
+        }
+    }
+}
